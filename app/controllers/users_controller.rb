a7a03610dd80a514d64324b3768abc197b0a4441
Pavel Smolov <palexsmol@gmail.com>
2018-12-28 23:26:21 -0800

    add login upon sing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,9 @@ before_action :require_same_user, only: [:edit, :update]
         @user = User.new(user_params)
 
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to the blog #{@user.username}"
-            redirect_to posts_path
+            redirect_to user_path(@user)
         else
             render :new
         end
