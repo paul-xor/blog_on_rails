db150fb0a8d8d75f275a4fc9b76a77c5987b3bb1
Pavel Smolov <palexsmol@gmail.com>
2019-01-05 23:53:30 -0800

    imp ckeditor and image upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   root 'pages#home'
   get 'pages/about', to: 'pages#about'
   get 'welcome/home', to: "welcome#home"
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_29_224441) do
+ActiveRecord::Schema.define(version: 2019_01_06_071225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string "data_file_name", null: false
+    t.string "data_content_type"
+    t.integer "data_file_size"
+    t.string "type", limit: 30
+    t.integer "width"
+    t.integer "height"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -21,6 +41,7 @@ ActiveRecord::Schema.define(version: 2018_12_29_224441) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +53,5 @@ ActiveRecord::Schema.define(version: 2018_12_29_224441) do
     t.boolean "admin", default: false
   end
 
+  add_foreign_key "comments", "posts"
 end
