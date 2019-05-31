461cd153119021df3904fb16f064e4f8368e8756
Pavel Smolov <palexsmol@gmail.com>
2019-05-30 21:19:54 -0700

    category validation up to 35 symbols and post show changed

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
     has_many :post_categories
     has_many :posts, through: :post_categories
-    validates :name, presence: true, length: { minimum: 3, maximum: 20 }
+    validates :name, presence: true, length: { minimum: 3, maximum: 35 }
     validates_uniqueness_of :name
 end
