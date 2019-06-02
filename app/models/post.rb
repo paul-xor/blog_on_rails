d666765587b11bc6de7bd97e909d37ff82a88ba2
Pavel Smolov <palexsmol@gmail.com>
2019-06-02 13:09:28 -0700

    add summary field to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
     has_many :post_categories
     has_many :categories, through: :post_categories
     validates :title, presence: true, length: {minimum: 3, maximum: 50}
+    validates :summary, presence: true, length: {minimum: 10, maximum: 200}
     validates :body, presence: true, length: {minimum: 10, maximum: 10000}
     validates :user_id, presence: true
     has_many(:comments, dependent: :destroy)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_032715) do
+ActiveRecord::Schema.define(version: 2019_06_02_184728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_06_02_032715) do
     t.datetime "updated_at"
     t.integer "user_id"
     t.string "image"
+    t.string "summary"
   end
 
   create_table "users", force: :cascade do |t|
